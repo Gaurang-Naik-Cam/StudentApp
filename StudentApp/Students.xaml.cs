3d740a45fdedb3bc96d70645e9af16d00d7e726d
Gaurang Naik <A00250808@mycambrian.ca>
2023-08-11 11:40:05 -0400

    Student CRUD Operation is implemented
    Student CRUD Operation is implemented

--- StudentApp/Students.xaml.cs
+++ StudentApp/Students.xaml.cs
@@ -1,35 +1,39 @@
 using StudentApp.Models;
 using StudentApp.Database;
-using Java.Lang;
+//using Java.Lang;
 
 namespace StudentApp;
 
 public partial class Students : ContentPage
-{    //List<Course> _totalCourses
-    //{
-    //    get
-    //    {
-    //        StudentAppDatabase database = await StudentAppDatabase.Instance;
-    //        return database.GetCoursesAsync().Result;
-    //       // return _totalCourses;
-    //    }
-    //}
+{    
+    
+    List<string> _totalCourses { get;}
 
-	public Students()
-	{
-		InitializeComponent();
-        //pcrCourses.ItemsSource = _totalCourses;
+    public Students()
+    {
+        InitializeComponent();
+        _totalCourses = new List<string>(); 
+        //pcrCourses.ItemsSource = new List<string>() { " Test 1", "Test 2" };
+        StudentAppDatabase studentAppDatabase = new StudentAppDatabase();
+        var courses = studentAppDatabase.GetCoursesAsync().Result;
+        foreach (var course in courses)
+        {
+            _totalCourses.Add(course.CourseName);
+        }
+
+        pcrCourses.ItemsSource = _totalCourses;
     }
 
 
 
     private async void OnSaveClicked(object sender, EventArgs e)
     {
-        var _student = new Student();
-        _student.Name = txtbxName.Text;
-        _student.StudentNumber = txtbxStudentNumber.Text;
-        _student.Address = txtbxStudentAddress.Text;
+        var _student = (Student)BindingContext;//new Student();
         _student.CourseName = lblSelectedItem.Text;
+        //_student.Name = txtbxName.Text;
+        //_student.StudentNumber = txtbxStudentNumber.Text;
+        //_student.Address = txtbxStudentAddress.Text;
+        //_student.CourseName = lblSelectedItem.Text;
         StudentAppDatabase database = await StudentAppDatabase.Instance;
         await database.SaveStudentAsync(_student);
         await Navigation.PopAsync();
@@ -37,10 +41,12 @@ public partial class Students : ContentPage
 
     private async void OnDeleteClicked(object sender, EventArgs e)
     {
-        var _student = new Student();
-        _student.Name = txtbxName.Text;
-        _student.StudentNumber = txtbxStudentNumber.Text;
-        _student.Address = txtbxStudentAddress.Text;
+        //var _student = new Student();
+        //_student.Name = txtbxName.Text;
+        //_student.StudentNumber = txtbxStudentNumber.Text;
+        //_student.Address = txtbxStudentAddress.Text;
+        //_student.CourseName = lblSelectedItem.Text;
+        var _student = (Student)BindingContext;
         _student.CourseName = lblSelectedItem.Text;
         StudentAppDatabase database = await StudentAppDatabase.Instance;
         await database.DeleteStudentAsync(_student);
@@ -50,5 +56,17 @@ public partial class Students : ContentPage
     private async void OnCancelClicked(object sender, EventArgs e)
     {
         await Navigation.PopAsync();
+    }
+
+    private void OnDropDownChanged(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        int selectedIndex = picker.SelectedIndex;
+
+        if (selectedIndex != -1)
+        {
+            lblSelectedItem.Text = (string)picker.ItemsSource[selectedIndex];
+        }
+        //await Navigation.PopAsync();
     }
 }
